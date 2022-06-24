c5f0b917720a24f999648203c1a110dc16890b08
Thousandhack <1391053697@qq.com>
2022-06-24 23:03:18 +0800

    python 中的序列分类

---
+++ advanced_python/014_sequence_.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# __author__ hsz
+
+"""
+python 中的序列分类
+
+容器序列   list tuple  deque
+扁平序列   str,bytes,bytearray,array.array
+可变序列   list,deque,bytearray,array
+不可变     str,tuple,bytes
+"""
+
