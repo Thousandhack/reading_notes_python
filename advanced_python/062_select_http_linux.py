d1eb0383b727f5f40f3dfd3ce57b4f79a12bc50e
Thousandhack <1391053697@qq.com>
2022-08-15 23:43:28 +0800

    使用非阻塞io 完成http请求

---
+++ advanced_python/062_select_http_linux.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# __author__ hsz
+
+import socket
+from urllib.parse import urlparse
+from selectors import DefaultSelector, EVENT_READ, EVENT_WRITE
+
+# import select
+"""
+使用非阻塞io 完成http请求
+
+使用select完成http请求
+DefaultSelector 在windows下是select ,在linux下就是epoll
+所以这个程序需要linux在才不会异常
+"""
+
+selector = DefaultSelector()
+
+
+class Fetcher:
+
+    def __init__(self):
+        self.host = ""
+        self.path = ""
+        self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.data = b""
+
+    def recv_data(self, key):
+        d = self.conn.recv(1024)
+        if d:
+            print(d)
+            self.data += d
+        else:
+            selector.unregister(key.fd)
+            data = self.data.decode("utf8")
+            html_data = data.split("\r\n\r\n")[1]
+            print(html_data.encode(encoding="utf8"), "1111")
+            self.conn.close()
+
+    def connected(self, key):
+        selector.unregister(key.fd)
+        self.conn.send(
+            "GET {} HTTP/1.1\r\nHost:{}"
+            "\r\nConnection:close\r\n\r\n".format(self.path, self.host).encode("utf8"))
+        selector.register(self.conn.fileno(), EVENT_READ, self.recv_data)
+
+
+
+    def get_url(self, url):
+        """
+        通过socket请求html
+        :param url:
+        :return:
+        """
+        url = urlparse(url)
+        self.host = url.netloc
+        self.path = url.path
+        if self.path == "":
+            self.path = "/"
+        # 建立socket连接
+        self.conn.setblocking(False)
+        try:
+            self.conn.connect((self.host, 80))
+        except BlockingIOError as e:
+            pass
+        # 注册
+        # 注册文件描述符  事件（读事件，写事件）  回调函数(只写函数名称)
+        selector.register(self.conn.fileno(), EVENT_WRITE, self.connected)
+
+
+def my_loop():
+    """
+    事件循环： 不停的请求socket的状态并调用对应的函数
+    1.select 本身是不支持register模式的
+    2.socket 状态变化的回调需要自己开发完成的
+
+    :return:
+    """
+
+    while True:
+        res_ = selector.select()
+        for key, mask in res_:
+            call_back = key.data
+            call_back(key)
+
+    # 回到+ 事件循环 + select(poll/epoll)
+
+
+if __name__ == "__main__":
+    fetcher = Fetcher()
+    fetcher.get_url("https://www.baidu.com")
+    my_loop()
